e6fe9726564702addc88aa6688325152dab90b63
Torin Jacobson <torin@boardsmithembedded.com>
2015-11-05 10:57:23 +0000

    implement initial fully-functional enraibler in gpio_demo.py

---
+++ AudioDelay.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import pygst
+pygst.require("0.10")
+import gst
+import sys
+import time
+
+# Audio delay code thanks to d53richar on Ubuntu Forums.
+# http://ubuntuforums.org/showthread.php?t=1189559
+
+class AudioDelay:
+	def __init__(self):
+		## ALSA
+		self.delay_pipeline = gst.Pipeline("mypipeline")
+		self.audiosrc = gst.element_factory_make("jackaudiosrc", "audio")
+		self.delay_pipeline.add(self.audiosrc)
+		
+		## Queue
+		self.audioqueue = gst.element_factory_make("queue", "queue1")
+		self.audioqueue.set_property("max-size-time", 0)
+		self.audioqueue.set_property("max-size-buffers", 0)
+		self.audioqueue.set_property("max-size-bytes", 0)
+		self.audioqueue.set_property("leaky", "no")
+		
+		self.delay_pipeline.add(self.audioqueue)
+
+		## Audio Output
+		self.sink = gst.element_factory_make("autoaudiosink", "sink")
+		self.delay_pipeline.add(self.sink)
+
+		## Link the elements
+		self.audiosrc.link(self.audioqueue)
+		self.audioqueue.link(self.sink)
+
+	def begin_delay(self, delay_ms):
+		delay_ns = long( float(delay_ms) * 100000000 )
+		self.delay_pipeline.set_state(gst.STATE_PAUSED)
+		self.audioqueue.set_property("min-threshold-time", delay_ns)
+		self.delay_pipeline.set_state(gst.STATE_PLAYING)
+
+	def kill(self):
+		self.delay_pipeline.set_state(gst.STATE_NULL)
--- gpio_demo.py
+++ gpio_demo.py
@@ -3,12 +3,12 @@
 import time
 import RPi.GPIO as GPIO
 import Adafruit_CharLCD as LCD
+import AudioDelay
 
 GPIO_ENC_0 = 9
 GPIO_ENC_1 = 10
 GPIO_BUTTON_0 = 11
 
-direction = "no"
 update = False
 delay = 0
 
@@ -27,25 +27,21 @@ lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
 					lcd_columns, lcd_rows, lcd_backlight)
 
 def buttonEventHandler(pin):
-	print pin
+	global update
+	update = True
 
 def encoderEventHandler(pin):
-	global direction
 	global update
 	global delay
 
 	if (GPIO.input(9) == GPIO.input(10)):
-		direction = "cw"
 		delay = delay + 1
 	else:
-		direction = "ccw"
 		if (delay > 0):
 			delay = delay - 1
-	update = True
 
 def main():
-	
-
+	audiodelay = AudioDelay.AudioDelay()
 	# Use chip's GPIO numbering scheme
 	GPIO.setmode(GPIO.BCM)
 
@@ -61,15 +57,20 @@ def main():
 	global update
 	global delay
 	global lcd
+	delay_prev = -1
 	while True:
-		if (update):
-			print direction
-			update = False
+		if (delay != delay_prev):
 			# let's print the delay on the LCD for now.
 			lcd.clear()
-			lcd.message("delay = " + str(delay))
+			lcd.message("delay = " + str(float(delay)/10))
+			delay_prev = delay
+	
+			update = False
+			audiodelay.begin_delay(delay)
 		time.sleep(0.01)
+		
 
 	GPIO.cleanup()
+	audiodelay.kill()
 
 main()
